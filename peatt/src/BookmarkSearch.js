b1ffc91782f13a1be450975144815211401e110f
Andrew Marten Medina <andrew.medina@fox.com>
2018-08-08 17:40:41 -0700

    updated to ES2017

--- peatt/src/BookmarkSearch.js
+++ peatt/src/BookmarkSearch.js
@@ -1,47 +1,36 @@
-import React, { Component } from "react"
-var got = require("got") 
+import React, { Component } from "react";
+import got from "got";
 
 class BookmarkSearch extends Component {
-
     handleSearchBar = event => {
-        if( event.target.value ) {
-          got.get("https://api-staging.fox.com/fbc-content/v1_5/video?videoType=fullEpisode&q=" + event.target.value.toString())
-          .then( res => {
-            var vidObj = []
-            var resObject = JSON.parse(res.body);
-            for( var index = 0; index < resObject.totalItems; index++) {
-              vidObj.push({"uID": resObject.member[index].uID});
-            }
-            this.setState({ videos: vidObj});
-          })
-          .catch( err => {
-            console.log(err)
-          })
-        }
-        else {
+        if (event.target.value) {
+          got.get(`https://api-staging.fox.com/fbc-content/v1_5/video?videoType=fullEpisode&q=${event.target.value.toString()}`)
+            .then(res => {
+              let videos = [];
+              for (let i = 0; i < JSON.parse(res.body).totalItems; i++) videos.push({ uID: JSON.parse(res.body).member[i].uID });
+              this.setState({ videos });
+            })
+            .catch(err => console.error(err));
+        } else {
           got.get("http://localhost:3001/api/videos?page=1")
-          .then((res) =>{
-            var videoObject = JSON.parse(res.body)
-            this.setState({ videos: videoObject.videoList });
-            return;
-          })
-          .catch( function(err){
-            console.log('error:', err);
-          })
+            .then(res => {
+              this.setState({ videos: JSON.parse(res.body).videoList });
+              return;
+            })
+            .catch(err => console.error(err));
         }
-      }
+    }
 
     render() {
-        return (
-            <div className="form-group">
-                <input 
-                className="form-control"
-                type="text"
-                placeholder="Search Video"
-                onChange={this.handleSearchBar}
-                />
-            </div>
-        )
+      return (
+        <div className="form-group">
+            <input 
+              className="form-control"
+              type="text"
+              placeholder="Search Video"
+              onChange={this.handleSearchBar} />
+        </div>
+      )
     }
 }
-export default BookmarkSearch
+export default BookmarkSearch;
